3f2f4326936981ec51eb6e07a48cd83f10b9684c
Mehran Zahirodini <mehranzahir@outlook.com>
2022-06-25 03:03:15 +0430

    minor change

--- pages/api/edit-todo.ts
+++ pages/api/edit-todo.ts
@@ -7,7 +7,6 @@ const editTodo = async (req: NextApiRequest, res: NextApiResponse) => {
     if (!db.data) return res.status(500).json("No data");
     const { body } = req;
     const todoItem = db.data?.findIndex((todo) => todo.id === body.id);
-    if (!db.data[todoItem]) return res.status(500).json("No such Item");
     db.data[todoItem] = body;
     await db.write();
     return res.status(200).json("success");
